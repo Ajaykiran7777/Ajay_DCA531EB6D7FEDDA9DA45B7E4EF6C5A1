b08a0adeeffdab5b7b873f496543ad3d5ec0fbdd
Ajaykiran7777 <144518043+Ajaykiran7777@users.noreply.github.com>
2023-10-26 13:51:14 +0530

    Add files via upload

---
+++ Challenge 2.py
@@ -0,0 +1,10 @@
+def isleapyear(year): 
+  if (year % 4==0 and year % 1001!=0) or year % 400==0:          return True 
+  else:
+    return False
+    
+year = int(input("enter a year : "))
+if isleapyear(year):
+  print("{} is a leap year.".format(year))
+else:
+  print("{} is not a leap year.".format(year))
